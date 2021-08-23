31415b3ee25aa36f071c093eea94276be3b80b8c
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-08-23 23:03:11 +0500

    Day 7 starting python code

---
+++ Python/variables.py
@@ -0,0 +1,21 @@
+#  ----------------------- Day 7 ----------------------
+#  ----- Varibale and Main  -------------------------
+#  --- Date: 23.08.2021---------------------------------
+#  Author: Muhammad Arslan Shahzad, 
+#  ------------------------------------------------------
+
+def main():
+    print("hello world")
+    my__number = 12
+    print(my__number)
+    user__name = "Arslan"
+    print(user__name)
+
+if __name__ == "__main__":
+    main()
+
+
+# Sample output
+# hello world
+# 12
+# Arslan
