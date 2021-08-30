1d3d5bcea78fcc8d4e9317b48ea97a23fa25ed2f
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-08-30 22:09:59 +0500

    day 14/100 arrays in python

---
+++ Python/arrays.py
@@ -0,0 +1,27 @@
+#  ----------------------- Day 14 ----------------------
+#  ----- Arrays  -------------------------
+#  --- Date: 30.08.2021---------------------------------
+#  Author: Muhammad Arslan Shahzad, 
+#  ------------------------------------------------------
+
+def main():
+    arr = ['sunday','monday','tuesday','wednesday']
+    print(arr[0])
+    for i in arr:
+        print(i)
+    
+
+    print(len(arr))
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Sample output
+    # sunday
+    # sunday
+    # monday
+    # tuesday
+    # wednesday
+    # 4
