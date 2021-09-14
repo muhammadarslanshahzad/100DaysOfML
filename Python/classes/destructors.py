9d778a2ff22fe391bbab9534e33f0b302f0d2e53
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-09-14 19:15:32 +0500

    day 29/100 destructors

---
+++ Python/classes/destructors.py
@@ -0,0 +1,32 @@
+#  ----------------------- Day 29 ----------------------
+#  ----- Constructors in python classes  -------------------------
+#  --- Date: 14.09.2021---------------------------------
+#  Author: Muhammad Arslan Shahzad, 
+#  ------------------------------------------------------
+
+class Employee:
+
+	def __init__(self):
+		print('Employee created')
+
+	def __del__(self):
+		print("Destructor called")
+
+def Create_obj():
+	print('Making Object...')
+	obj = Employee()
+	print('function end...')
+	return obj
+
+print('Calling Create_obj() function...')
+obj = Create_obj()
+print('Program End...')
+
+
+# Sample Output
+    # Calling Create_obj() function...
+    # Making Object...
+    # Employee created
+    # function end...
+    # Program End...
+    # Destructor called
