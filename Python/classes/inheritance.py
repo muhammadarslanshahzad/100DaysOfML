764d2ae80bf5f9abdd116ae7ec949645d02016f1
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-09-15 16:56:13 +0500

    D30 inheritance

---
+++ Python/classes/inheritance.py
@@ -0,0 +1,34 @@
+#  ----------------------- Day 30 ----------------------
+#  ----- Constructors in python classes  -------------------------
+#  --- Date: 15.09.2021---------------------------------
+#  Author: Muhammad Arslan Shahzad, 
+#  ------------------------------------------------------
+
+# parent class
+class Person( object ):	
+
+		# __init__ is known as the constructor		
+		def __init__(self, name, idnumber):
+				self.name = name
+				self.idnumber = idnumber
+		def display(self):
+				print(self.name)
+				print(self.idnumber)
+
+# child class
+class Employee( Person ):		
+		def __init__(self, name, idnumber, salary, post):
+				self.salary = salary
+				self.post = post
+
+				Person.__init__(self, name, idnumber)
+
+				
+a = Employee('Arslan', 886012, 200000, "Intern")	
+
+a.display()
+
+
+# Sample Output
+    # Arslan
+    # 886012
